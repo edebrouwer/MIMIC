5d51d4ec010dabda74a349958e174dd34c35a3eb
Edward De Brouwer <edwarddebrouwer@Edwards-Machine.local>
2018-03-27 17:21:14 +0200

    Main.py for lynx

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#
+#This script runs the pytorch optimization on the real-world data (meant for the Lynx Server operation)
+
 import MIMICdev as dev
 import MIMICtorch as mtorch
 
 from MIMICtorch import EHRDataset
+from MIMICtorch import model_train
 
 import torch
 import numpy as np
@@ -14,35 +16,15 @@ print("Number of patients: "+str(X_source[2][0]))
 print("Number of conditions: "+str(X_source[2][1]))
 print("Number of time steps: "+str(X_source[2][2]))
 
-#pat=150
-#cond=150
-#K_train=2
-#U_train=np.random.randn(pat,K_train,1) #Patient,latent_dim,time
-#V_train=np.random.randn(K_train,cond) #latent_dim,condition
-#X_prod=np.dot(U_train,V_train)
-#X_prod=np.einsum('ijk,jl->ilk',U_train,V_train)
-#X_prob=mtorch.sigmoid(X_prod)
-#X_bin=np.random.binomial(1,X_prob)
-#X_ids=np.asarray(np.where(~np.isnan(X_bin)))
-#X_source=(X_ids,X_bin[tuple(X_ids)],X_bin.shape)
-
-
-#X_source=np.ones((10,10,10))
 
 print("Loading data ... ")
-[Xtrain,Xval,Xtest]=mtorch.train_test(X_source,0.15,0.05)
+[Xtrain,Xval,Xtest]=mtorch.train_test(X_source,0.2,0.1)
 ehr=EHRDataset(Xtrain)
 print("Number of data points : "+str(len(Xtrain[1])))
-print("Data Loaded !")
+print("Data loaded !")
 
-#print("Pre-processing data")
-#[X_train,X_mask,Xtest]=mtorch.pre_proc_data(X_source,prop=0.2)
-#print("Pre-processing done !")
-
-#training:
-print("Training ... ")
-[U,V]=mtorch.run_training(ehr,Xval,sig2=4,K=2,l_r=0.01,batch_size=100)
-print("Done with training ! ")
+mod=model_train(ehr,Xval,l_r=0.005,epochs_num=500,batch_size=1000,sig2_prior=2,K=2,check_freq=20,l_kernel=3,kernel_type="square-exp")
+[U,V]=mod.run_train()
 
 #train recap :
 train_loss=mtorch.test_loss(Xtrain,U,V)
@@ -60,10 +42,15 @@ print("Baseline :"+str(1-sum(Xtest[1])/len(Xtest[1])))
 Unp=U.data.numpy()
 Vnp=V.data.numpy()
 
-X_prob_inf=mtorch.sigmoid(np.einsum('ijk,jl->ilk',Unp,Vnp))[tuple(Xtest[0])]
-print('Mean difference of test probabilities : '+str(np.mean(np.abs(X_prob_inf-X_prob[tuple(Xtest[0])]))))
 
 np.save("Utorch",Unp)
 np.save("Vtorch",Vnp)
+
+np.save("Utrain",U_train)
+np.save("Vtrain",V_train)
+
 np.save("Xtrain",Xtrain)
 np.save("Xtest",Xtest)
+
+np.save("Xtrain_hist",mod.Train_history)
+np.save("Xval_hist",mod.Val_history)
