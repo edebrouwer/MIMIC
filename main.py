8a0bd6d9546553757935920f98dd5d5f5faea991
Edward De Brouwer <edebrouwer@lab.exascience.com>
2018-04-04 17:29:34 +0200

    removed Plots"

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 import MIMICdev as dev
 import MIMICtorch as mtorch
 
-from MIMICtorch import EHRDataset
+from MIMICtorch import EHRDataset,EHRDataset3
 from MIMICtorch import model_train
 
 import torch
 import numpy as np
+
+import os.path as path
 
 print("Sourcing Data ... ")
 X_source=dev.matrix_creation() # Tuple with index and data of the matrix source.
@@ -20,26 +22,19 @@ print("Number of time steps: "+str(X_source[2][2]))
 print("Loading data ... ")
 [Xtrain,Xval,Xtest]=mtorch.train_test(X_source,0.2,0.1)
 
-file_dat=path.join('./fpdata.dat')
-file_idx=path.join('./fpidx.dat')
-fp_dat=np.memmap(file_dat,mode="w+",shape=Xtrain[1].shape)
-fp_dat[:]=Xtrain[1]
-fp_idx=np.memmap(file_idx,mode="w+",shape=Xtrain[0].shape)
-fp_idx[:]=Xtrain[0]
-
 #ehr=EHRDataset(Xtrain)
-ehr=EHRDataset2(file_dat,file_idx,Xtrain[2],len(Xtrain[0][0]))
+ehr=EHRDataset3(Xtrain)
 print("Number of data points : "+str(len(Xtrain[1])))
 print("Data loaded !")
 
-del Xtrain
+#del Xtrain
 
-mod=model_train(ehr,Xval,l_r=0.005,epochs_num=500,batch_size=10000,sig2_prior=2,K=2,check_freq=50,l_kernel=5,kernel_type="square-exp")
+mod=model_train(ehr,Xval,l_r=0.005,epochs_num=500,batch_size=2000,sig2_prior=2,K=2,check_freq=50,l_kernel=5,kernel_type="square-exp")
 [U,V]=mod.run_train()
 
-#train recap :
-train_loss=mtorch.test_loss(Xtrain,U,V)
-print("Overall train loss: "+str(train_loss))
+##train recap :
+#train_loss=mtorch.test_loss(Xtrain,U,V)
+#print("Overall train loss: "+str(train_loss))
 #val recap :
 val_loss=mtorch.test_loss(Xval,U,V)
 print("Overall validation loss: "+str(val_loss))
@@ -57,8 +52,8 @@ Vnp=V.data.numpy()
 np.save("Utorch",Unp)
 np.save("Vtorch",Vnp)
 
-np.save("Utrain",U_train)
-np.save("Vtrain",V_train)
+#np.save("Utrain",U_train)
+#np.save("Vtrain",V_train)
 
 np.save("Xtrain",Xtrain)
 np.save("Xtest",Xtest)
